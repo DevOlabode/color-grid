703574efb08c6603d31b773d39c5c8c6e910e2fa
Samuel Olabode <Solabode499@gmail.com>
2025-11-25 16:30:56 -0700

    `Added CSS import and repeated ColorBox components`

--- src/ColorBoxGrid.jsx
+++ src/ColorBoxGrid.jsx
@@ -1,8 +1,31 @@
+import './ColorBoxGrid.css'
 import ColorBox from './ColorBox'
 
 export default function ColorBoxGrid({colors}){
     return (
         <div className="ColorBoxGrid">
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
+            <ColorBox colors={colors} />
             <ColorBox colors={colors} />
             <ColorBox colors={colors} />
             <ColorBox colors={colors} />
